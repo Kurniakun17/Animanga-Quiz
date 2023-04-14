5f8733df84e8c4e746b665b7860796f3451bc406
Kurnia Kharisma <kurniakharisma17@gmail.com>
2023-04-14 13:50:44 +0700

    test

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,21 +1,17 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
   },
-  extends: [
-    'plugin:react/recommended',
-    'standard-with-typescript'
-  ],
-  overrides: [
-  ],
+  extends: ['plugin:react/recommended', 'standard-with-typescript', 'prettier'],
+  overrides: [],
   parserOptions: {
     ecmaVersion: 'latest',
-    sourceType: 'module'
+    sourceType: 'module',
+    project: 'tsconfig.json',
   },
-  plugins: [
-    'react'
-  ],
+  plugins: ['react'],
   rules: {
-  }
-}
+    '@typescript-eslint/explicit-function-return-type': 'off',
+  },
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
+import {Routes, Route} from 'react-router-dom';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
-    </div>
-  )
+  <div className="App">
+    <Routes>
+      <Route></Route>
+    </Routes>
+  </div>);
 }
 
-export default App
+export default App;
