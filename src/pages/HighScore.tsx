d6d44523b00ee35a9d40bce6360cd1b7f96740a2
Kurnia Kharisma <kurniakharisma17@gmail.com>
2023-04-15 20:29:20 +0700

    fixing highscores page for movbile

--- src/pages/HighScore.tsx
+++ src/pages/HighScore.tsx
@@ -10,7 +10,9 @@ interface HighScoreProps extends QuizResultProps {
 
 export const HighScore = () => {
   const navigate = useNavigate();
-  const highScoreRef = useRef<[] | HighScoreProps[]>(getHighScoreFromLocalStorage());
+  const highScoreRef = useRef<[] | HighScoreProps[]>(
+    getHighScoreFromLocalStorage()
+  );
 
   const onBackHandler = () => {
     navigate('/main-menu');
@@ -25,33 +27,35 @@ export const HighScore = () => {
         >
           Back
         </button>
-        <h1 className="text-3xl font-bold text-center mb-2 text-[#00C8B4]">
+        <h1 className="text-1xl font-bold text-center mb-2 text-[#00C8B4]">
           High Score
         </h1>
         {highScoreRef.current.length === 0 ? (
           <h1 className="font-bold text-center text-3xl my-4">No History</h1>
         ) : (
-          <table className="border-solid border-2 border-slate-500 w-full text-center">
+          <table className="border-solid border-2 border-slate-500 w-full text-center table-fixed">
             <thead className="rounded-lg">
               <tr>
-                <th className="p-2 bg-gray-500">Date</th>
-                <th className="p-2 bg-gray-500">Correct</th>
-                <th className="p-2 bg-gray-500">Wrong</th>
-                <th className="p-2 bg-gray-500">Unanswered</th>
-                <th className="p-2 bg-gray-500">Scores</th>
+                <th className="p-2 bg-gray-500 text-xs">Date</th>
+                <th className="p-2 bg-gray-500 text-xs">Correct</th>
+                <th className="p-2 bg-gray-500 text-xs">Wrong</th>
+                <th className="p-2 bg-gray-500 text-xs">Unanswered</th>
+                <th className="p-2 bg-gray-500 text-xs">Scores</th>
               </tr>
             </thead>
             <tbody>
               {highScoreRef.current
-                .sort((a: HighScoreProps, b: HighScoreProps) => b.score - a.score)
+                .sort(
+                  (a: HighScoreProps, b: HighScoreProps) => b.score - a.score
+                )
                 .map((data: HighScoreProps, index: number) => {
                   return (
                     <tr key={`highscore-${index}`}>
-                      <td className="p-2">{data.date}</td>
-                      <td className="p-2">{data.correct}</td>
-                      <td className="p-2">{data.wrong}</td>
-                      <td className="p-2">{data.unAnswered}</td>
-                      <td className="p-2">{data.score}</td>
+                      <td className="p-2 text-[10px]">{data.date}</td>
+                      <td className="p-2 text-[10px]">{data.correct}</td>
+                      <td className="p-2 text-[10px]">{data.wrong}</td>
+                      <td className="p-2 text-[10px]">{data.unAnswered}</td>
+                      <td className="p-2 text-[10px]">{data.score}</td>
                     </tr>
                   );
                 })}
