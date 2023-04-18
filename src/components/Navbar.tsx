9d675bcb98282d2fdfb8735b1d347f120a61ef40
Kurnia Kharisma <kurniakharisma17@gmail.com>
2023-04-19 02:32:08 +0700

    changing the navbar color to pure white

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -21,9 +21,9 @@ export const Navbar = ({ user, setUser, setQuizResult }: NavbarProps) => {
   };
 
   return (
-    <div className="w-full p-4 px-0 bg-[#2D3346] font-bold">
+    <div className="w-full p-4 px-0 bg-white dark:bg-[#2D3346] font-bold">
       <div className="flex justify-between items-center w-[90%] m-auto">
-        <h2 className="">Hai, {user}</h2>
+        <h2 className="text-[#2d3346] dark:text-white lg:text-xl">Hai, {user}</h2>
         <button
           className="px-3 py-1 bg-red-600 rounded-lg"
           onClick={onLogoutHandler}
